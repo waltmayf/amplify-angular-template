dde2f5263c2626971caec95a088427e06d4c4af1
waltmayf <126521508+waltmayf@users.noreply.github.com>
2026-04-03 11:25:55 -0500

    Fixed complex querie and added auth0 auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,20 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      oidc: [
+        {
+          name: 'Auth0',
+          clientId: secret('AUTH0_CLIENT_ID'),
+          clientSecret: secret('AUTH0_CLIENT_SECRET'),
+          issuerUrl: 'https://dev-n27lv88dihjzwt0j.us.auth0.com',
+          scopes: ['openid', 'profile', 'email'],
+        },
+      ],
+      callbackUrls: ['http://localhost:4200'],
+      logoutUrls: ['http://localhost:4200'],
+    },
   },
 });
